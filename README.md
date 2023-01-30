6dfc4bf75b1b1730eda7544f3ab185fdded31b96
ishtiaqhimel <ishtiaq@appscode.com>
2023-01-30 19:11:33 +0600

    updated crd-controller

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# crd-controller
+# CRD Controller
+Guidelines for writing controllers - [Link](https://github.com/kubernetes/community/blob/master/contributors/devel/sig-api-machinery/controllers.md)
--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	clientset "github.com/ishtiaqhimel/crd-controller/pkg/client/clientset/versioned"
+	informers "github.com/ishtiaqhimel/crd-controller/pkg/client/informers/externalversions"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"log"
+	"path/filepath"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello")
+	log.Println("Configure KubeConfig...")
+
+	var kubeconfig *string
+
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	exampleClient, err := clientset.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	kubeInformationFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	exampleInformationFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+	//controller := NewController()
 }
